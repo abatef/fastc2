f78c0999f8073b33d967a86829083347ea43b60b
abatef <abdoatef.ab@gmail.com>
2025-04-04 19:06:08 +0200

    updated error handling

--- src/main/java/com/abatef/fastc2/exceptions/NonExistingValueException.java
+++ src/main/java/com/abatef/fastc2/exceptions/NonExistingValueException.java
@@ -1,7 +1,7 @@
 package com.abatef.fastc2.exceptions;
 
 import com.abatef.fastc2.enums.ValueType;
-import com.abatef.fastc2.utils.Constants;
+import com.abatef.fastc2.utils.Values;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +15,7 @@ public class NonExistingValueException extends RuntimeException {
     private String value;
 
     public NonExistingValueException(ValueType valueType, String value) {
-        super(String.format(Constants.NONEXISTENT_VALUE, valueType.name().toLowerCase(), value));
+        super(String.format(Values.NONEXISTENT_VALUE, valueType.name().toLowerCase(), value));
         this.valueType = valueType;
         this.value = value;
     }
