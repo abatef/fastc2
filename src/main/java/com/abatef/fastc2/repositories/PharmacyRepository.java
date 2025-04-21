41857e933c45170ef425349d804ceb41923daa39
abatef <abdoatef.ab@gmail.com>
2025-04-21 17:22:30 +0200

    updated the UserService

--- src/main/java/com/abatef/fastc2/repositories/PharmacyRepository.java
+++ src/main/java/com/abatef/fastc2/repositories/PharmacyRepository.java
@@ -10,12 +10,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Integer> {
     Optional<Pharmacy> getPharmacyById(Integer id);
 
+    List<Pharmacy> getPharmaciesByOwner_Id(Integer ownerId);
     @Query(
             value =
                     "select * from pharmacies p"
